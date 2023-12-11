5a64da95422b98a8c687dc7a567b61fcd920cdc0
beankhanh <beankhanh2001@gmail.com>
2023-12-11 08:52:03 +0700

    chap2, video9

---
+++ video9/video9.js
@@ -0,0 +1,12 @@
+let obj={
+    name:'khánh',
+    age:'23'
+};
+console.log(`obj:`,obj);
+let arr = ['Nam','hải','tú']
+console.log(`arr:`, arr)
+arr=[
+    {obj},
+    {name:'Khải',age:'30'}
+]
+console.log(`arr:`, arr)
